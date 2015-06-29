0713efc20eaa79efeeadc8ef315450d191a1ffc2
Bradley D. Johnson <bradley.david.johnson@gmail.com>
2015-06-29 10:18:44 -0700

    Rename Twilio controller to Notifications.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :requests
 
 
-  get '/messages' => 'twilio#incoming'
+  get '/messages' => 'notifications#incoming'
   # get "/school" => "schools#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
