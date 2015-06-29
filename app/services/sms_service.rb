98db2ee4785954b907d3edd75aa53121deae3197
Bradley D. Johnson <bradley.david.johnson@gmail.com>
2015-06-29 13:16:16 -0700

    Revision to SMS service, now including request ID

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -10,7 +10,11 @@ class SmsService
     @client.messages.create(
       from: Rails.application.secrets.twilio_number,
       to: @teacher.phone_number,
-      body: "Hello, #{@teacher.full_name}.\n\n#{@school.school_name} at #{@school.address} has an opening to teach #{@request.format_grade} on #{@request.format_date}.\nPlease respond yes to this text if you are available."
+      body: "Hello, #{@teacher.full_name}.\n\n"\
+      "#{@school.school_name} at #{@school.address} has an opening "\
+      "to teach #{@request.format_grade} on #{@request.format_date}.\n\n"\
+      "This is request ##{@request.id}."\
+      "Please respond 'yes #{@request.id}' to this text if you are available."
     )
   end
 end
