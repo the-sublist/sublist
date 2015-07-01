82ee84e10328795e424629183a81776488d0b244
Bradley D. Johnson <bradley.david.johnson@gmail.com>
2015-07-01 12:57:31 -0700

    ActiveRecord Relation

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,7 @@
 class OffersController < ApplicationController
   def send_confirmation
     @offer = Offer.where(id: params[:offer][:id]).includes(:teacher, :request)
-    @offer.update_attribute(:confirmation_sent, true)
+    @offer[0].update_attribute(:confirmation_sent, true)
     ConfirmationService.new(@offer)
     render json: @offer
   end
